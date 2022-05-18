f7e3342a399b2df64daf884fd596281fae504c66
matheusmota3242 <matheusmota3242@gmail.com>
2022-05-18 11:49:26 -0300

    primeiro commit

---
+++ src/main/java/br/ufrn/imd/clientereclamacoes/ClienteReclamacoesApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.clientereclamacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteReclamacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteReclamacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/clientereclamacoes/ClienteReclamacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.clientereclamacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteReclamacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
